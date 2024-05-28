855e81e5a1e42e4165faa86012e5dac564d46a44
paulbasuakabeya <135709152+paulbasuakabeya@users.noreply.github.com>
2024-05-28 13:52:23 +0100

    animation background

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homePages';
-import  BackgroundText from './components/background'
+// import  BackgroundText from './components/background'
 const App = () => {
   return (
-    <div className="bg-customGreen relative h-screen">
-       <BackgroundText />
-    <Router >
-       <h1 className="">
-      ESMICOM 2022
-      </h1>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+
+    <div className="relative h-screen bg-customGreen overflow-hidden">
+      <div className="background-text">ESMICOM 2022</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
+    // <div className="bg-customGreen relative h-screen">
+    //    <BackgroundText />
+    // <Router >
+    //    <h1 className="">
+    //   ESMICOM 2022
+    //   </h1>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //   </Routes>
+    // </Router>
+    // </div>
   );
 };
 
